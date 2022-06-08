81950a216baf39b6a4012d8815323c966b72d6ec
Teddy Byron <ted@tedbyron.com>
2022-06-08 00:20:27 -0400

    stuff

--- build.zig
+++ build.zig
@@ -4,24 +4,22 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    const include = [_][]const u8{"libs/mbedtls/include"};
-
     const lib = b.addStaticLibrary("mbedtls", null);
     lib.setTarget(target);
     lib.setBuildMode(mode);
     if (lib.target.isWindows()) {
         lib.linkSystemLibrary("ws2_32");
     }
+    lib.addIncludeDir("libs/mbedtls/include");
+    lib.addIncludeDir("libs/mbedtls/library");
+    lib.addCSourceFile("libs/mbedtls/library/rsa.c", &[_][]const u8{});
+    lib.addCSourceFile("libs/mbedtls/library/sha256.c", &[_][]const u8{});
     lib.linkLibC();
-    for (include) |path| {
-        lib.addIncludeDir(path);
-    }
     lib.install();
 
     const exe = b.addExecutable("mbedtls-rsa", null);
     exe.setTarget(target);
     exe.setBuildMode(mode);
-
     exe.addIncludeDir("libs/mbedtls/include");
     exe.addIncludeDir("libs/mbedtls/library");
     exe.addCSourceFile("src/main.c", &[_][]const u8{});
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "mbedtls/rsa.h"
 #include "mbedtls/sha256.h"
@@ -26,7 +27,7 @@ void read_key(const char *restrict file, char *restrict buf) {
 }
 
 int main(void) {
-  const char *str = "Hello World!";
+  const unsigned char input[] = "Hello World!";
   char *pub_key;
   char *priv_key;
   unsigned char hash[32];
@@ -36,7 +37,17 @@ int main(void) {
   read_key("./src/public.pem", pub_key);
   read_key("./src/private.pem", priv_key);
 
+  mbedtls_sha256_init(&sha256_ctx);
+  mbedtls_sha256_starts_ret(&sha256_ctx, 0);
+  mbedtls_sha256_update_ret(&sha256_ctx, input, strlen((char *)input));
+  mbedtls_sha256_finish_ret(&sha256_ctx, hash);
+  printf("%s\n", hash);
   mbedtls_sha256_free(&sha256_ctx);
+
+  mbedtls_rsa_init(&rsa_ctx, MBEDTLS_RSA_PKCS_V21, MBEDTLS_MD_SHA256);
+  mbedtls_rsa_rsassa_pss_verify(&rsa_ctx, NULL, NULL, MBEDTLS_RSA_PUBLIC,
+                                MBEDTLS_MD_SHA256, 32, hash, sig);
+
   mbedtls_rsa_free(&rsa_ctx);
   return 0;
 }
